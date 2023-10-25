bd473b7c20eb9b5be55049897c85ac8de95ae00d
Isaac Patka <ipatka@gmail.com>
2023-10-24 23:17:55 -0400

    fix deps

--- index.ts
+++ index.ts
@@ -1,17 +1,3 @@
-// Copyright Pravega Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// One line destructuring a CommonJs module is not possible. Break into two lines.
 import node_pre_gyp from '@mapbox/node-pre-gyp';
 const { find } = node_pre_gyp;
 import { resolve, join, dirname } from 'path';
